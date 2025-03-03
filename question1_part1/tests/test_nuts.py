35e07bb73d02ee4901f61407714f75b49b3096a5
Shuo HAO <shuohaopolyu@gmail.com>
2025-03-03 15:13:37 +0800

    test for NUTS

--- question1_part1/tests/test_nuts.py
+++ question1_part1/tests/test_nuts.py
@@ -46,36 +46,49 @@ class TestNoUTurnSampling(unittest.TestCase):
         q_prime, p_prime = self.nuts.leapfrog(q0, p0, -1)
         self.assertEqual(q_prime.shape, q0.shape)
         self.assertEqual(p_prime.shape, p0.shape)
-        
-    # def test_call(self):
-    #     q0 = tf.constant([[0.0]])
-    #     nuts = NoUTurnSampling(num_samples=500, q0=q0, dt=0.05, lhnn=self.lhnn)
-    #     nuts()
-    #     # q_hist = tf.concat(nuts.q_hist, axis=0)
-    #     # plt.hist(q_hist.numpy()[1000:, 0].flatten(), bins=30)
-    #     # plt.show()
-    #     self.assertEqual(len(nuts.q_hist), nuts.num_samples + 1)
 
-    # def test_buildtree(self):
-    #     q0 = tf.constant([[0.0]])
-    #     p0 = tf.constant([[1.0]])
-    #     H = self.lhnn.forward(q0, p0)
-    #     u = tf.random.uniform([], 0, tf.exp(-H))
-    #     nuts = NoUTurnSampling(num_samples=200, q0=q0, dt=0.01, lhnn=self.lhnn)
-    #     q_minus, p_minus, q_plus, p_plus, C_prime, s_prime = nuts.buildtree(
-    #         q0, p0, u, 1, 5
-    #     )
-    #     C = tf.concat([c[0] for c in C_prime], axis=0)
-    #     symplectic = (self.lhnn.symplectic_integrate(q0, p0, 0.01, 32))[1:, :1]
-    #     # plt.plot(symplectic.numpy())
-    #     # plt.plot(C.numpy())
-    #     # plt.show()
-    #     # compare the leapfrog integrator with the symplectic integrator
-    #     self.assertTrue(
-    #         tf.reduce_mean(tf.abs(symplectic - C)) < 1e-4,
-    #         "The leapfrog integrator is not accurate.",
-    #     )
-    #     self.assertEqual(q_minus.shape, q0.shape)
-    #     self.assertEqual(p_minus.shape, p0.shape)
-    #     self.assertEqual(q_plus.shape, q0.shape)
-    #     self.assertEqual(p_plus.shape, p0.shape)
+    def test_buildtree(self):
+        q0 = tf.constant([[0.0]])
+        p0 = tf.constant([[1.0]])
+        H = self.lhnn.forward(q0, p0)
+        u = tf.random.uniform([], 0, tf.exp(-H))
+        nuts = NoUTurnSampling(
+            num_samples=200,
+            q0=q0,
+            dt=0.01,
+            lhnn=self.lhnn,
+            Hamiltonian=self.Hamiltonian,
+            Delta_lf=1000.0,
+            Delta_lhnn=10.0,
+            num_lf_steps=20,
+        )
+        (
+            q_minus,
+            p_minus,
+            q_plus,
+            p_plus,
+            q_prime,
+            p_prime,
+            n_prime,
+            s_prime,
+            indicator_lf,
+        ) = nuts.buildtree(q0, p0, u, 1, 5, indicator_lf=0)
+        symplectic = (self.lhnn.symplectic_integrate(q0, p0, 0.01, 32))[1:, :1]
+        self.assertEqual(q_minus[0,0], symplectic[0,0])
+        self.assertEqual(q_plus[0,0], symplectic[-1,0])
+        self.assertEqual(q_minus.shape, q0.shape)
+        self.assertEqual(p_minus.shape, p0.shape)
+        self.assertEqual(q_plus.shape, q0.shape)
+        self.assertEqual(p_plus.shape, p0.shape)
+        self.assertEqual(q_prime.shape, q0.shape)
+        self.assertEqual(p_prime.shape, p0.shape)
+        self.assertEqual(n_prime.shape, (1,))
+        self.assertEqual(s_prime.shape, (1,))
+        self.assertIsInstance(indicator_lf, int)
+        
+
+    def test_call(self):
+        q0 = tf.constant([[0.0]])
+        nuts = NoUTurnSampling(num_samples=10, q0=q0, dt=0.05, lhnn=self.lhnn, Hamiltonian=self.Hamiltonian, Delta_lf=1000.0, Delta_lhnn=10.0, num_lf_steps=20)
+        res = nuts()
+        self.assertEqual(res.shape, (nuts.num_samples + 1, 1))
